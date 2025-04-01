38e1e5478ac32ab33eaa7fb3c49469c03a82fac3
Fadel Muzahdi <padel.muzahdi@gmail.com>
2025-03-31 23:57:01 -0400

    Added TodoListItem Component and Implemented useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import { useState } from 'react';
 import './App.css';
 import TodoList from './TodoList';
 import TodoForm from './TodoForm';
 
 function App() {
+  const [newTodo, setNewTodo] = useState(TodoList.todo);
+
   return (
     <div>
       <h1>My Todos</h1>
-      <TodoForm/>
-      <TodoList/>
+      <TodoForm />
+      <p>newTodo</p>
+      <TodoList />
     </div>
   );
 }
