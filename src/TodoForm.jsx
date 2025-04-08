a3a4c4c8eee1ac6c217dc64e2a63e38b32dca00e
Fadel Muzahdi <padel.muzahdi@gmail.com>
2025-04-08 19:12:09 -0400

    Added functionality to add todo items with unique id for each entry and improved code readability.

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,8 +1,32 @@
-function TodoForm() {
+import { useRef } from 'react';
+
+// personal note: TodoForm receives onAddTodo as a prop so we can access it.
+function TodoForm({ onAddTodo }) {
+  const todoTitleInput = useRef('');
+
+  function handleAddTodo(event) {
+    event.preventDefault();
+
+    const title = event.target.title.value;
+    // this is prop passed from App.jsx (the parent) to the child (TodoForm.jsx).
+    // it gets whatever is entered in the <input> into the onAddTodo function and passes it back to the parent.
+    // then, the parent re-renders the state with the new update.
+
+    const newTodo = {
+      title: title,
+      id: Date.now()
+    };
+
+    onAddTodo(newTodo);
+
+    event.target.title.value = '';
+    todoTitleInput.current.focus();
+  }
+
   return (
-    <form>
+    <form onSubmit={handleAddTodo}>
       <label htmlFor="todoTitle">To Do: </label>
-      <input id="todoTitle"></input>
+      <input ref={todoTitleInput} name="title" id="todoTitle"></input>
       <button type="submit">Add To Do</button>
     </form>
   );
