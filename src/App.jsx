124b1b02650dce9d4a88460218f670cb0c0bec6b
Fadel Muzahdi <padel.muzahdi@gmail.com>
2025-04-15 21:20:07 -0400

    Added a conditional rendering when todoList is empty, disabled the button element when input box is empty, allow users to check items off the list, and converted form into a controlled component.

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,27 @@ function App() {
   // personal note: the handleAddTodo function is defined in the parent.
   // then, it is passed to the child so that the child can tell the parent to do something. Ex: <TodoForm onAddTodo={handleAddTodo} />
   // when the child gets a new todo, it tells the handleAddTodo in the parent to add it to the state. Ex. callback invocation
-  function handleAddTodo(newTodo) {
+
+  function addTodo(newTodo) {
     setTodoList([...todoList, newTodo]);
+  }
+
+  function completeTodo(id) {
+    const updatedTodos = todoList.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, isCompleted: true };
+      }
+      return todo;
+    });
+
+    setTodoList(updatedTodos);
   }
 
   return (
     <div>
       <h1>My Todos</h1>
-      <TodoForm onAddTodo={handleAddTodo} />
-      <TodoList todoList={todoList} />
+      <TodoForm onAddTodo={addTodo} />
+      <TodoList todoList={todoList} onCompleteTodo={completeTodo} />
     </div>
   );
 }
