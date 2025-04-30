26183e5f991e45b4be26d59de9d0f4c842c03e4c
Fadel Muzahdi <padel.muzahdi@gmail.com>
2025-04-29 22:55:39 -0400

    Established Airtable database, fetch todos from Airtable on load, display pending updates during network requests, and display error messages on network or auth error.

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,227 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import TodoList from './features/TodoList/TodoList';
 import TodoForm from './features/TodoForm';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
-  // personal note: the handleAddTodo function is defined in the parent.
-  // then, it is passed to the child so that the child can tell the parent to do something. Ex: <TodoForm onAddTodo={handleAddTodo} />
-  // when the child gets a new todo, it tells the handleAddTodo in the parent to add it to the state. Ex. callback invocation
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [isSaving, setIsSaving] = useState(false);
 
-  function addTodo(newTodo) {
-    setTodoList([...todoList, newTodo]);
-  }
+  const url = `https://api.airtable.com/v0/${import.meta.env.VITE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+  const token = `Bearer ${import.meta.env.VITE_PAT}`;
 
-  function completeTodo(id) {
-    const updatedTodos = todoList.map((todo) => {
-      if (todo.id === id) {
-        return { ...todo, isCompleted: true };
+  useEffect(() => {
+    const fetchTodos = async () => {
+      setIsLoading(true);
+
+      const options = {
+        method: 'GET',
+        headers: {
+          Authorization: token,
+        },
+      };
+
+      try {
+        const resp = await fetch(url, options);
+
+        if (!resp.ok) {
+          throw new Error(resp.message);
+        }
+
+        const data = await resp.json();
+        const fetchedData = data.records.map((record) => {
+          const todo = {
+            id: record.id,
+            ...record.fields,
+          };
+          if (!todo.isCompleted) {
+            todo.isCompleted = false;
+          }
+          return todo;
+        });
+        setTodoList([...fetchedData]);
+      } catch (error) {
+        setErrorMessage(error.message);
+      } finally {
+        setIsLoading(false);
       }
-      return todo;
-    });
+    };
+    fetchTodos();
+  }, []);
 
-    setTodoList(updatedTodos);
-  }
+  // personal note: the addTodo function is defined in the parent.
+  // then, it is passed to the child so that the child can tell the parent to do something. Ex: <TodoForm onAddTodo={addTodo} />
+  // when the child gets a new todo, it tells the addTodo in the parent to add it to the state. Ex. callback invocation
+  const addTodo = async (newTodo) => {
+    const payload = {
+      records: [
+        {
+          fields: {
+            title: newTodo.title,
+            isCompleted: newTodo.isCompleted,
+          },
+        },
+      ],
+    };
 
-  function updateTodo(editedTodo) {
-    const updatedTodos = todoList.map((todo) => {
-      if (todo.id === editedTodo.id) {
-        return { ...editedTodo };
+    const options = {
+      method: 'POST',
+      headers: {
+        Authorization: token,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    };
+
+    try {
+      setIsSaving(true);
+      const resp = await fetch(url, options);
+
+      if (!resp.ok) {
+        throw new Error(resp.message);
       }
-      return todo;
-    });
 
-    setTodoList(updatedTodos);
-  }
+      const { records } = await resp.json();
+      const savedTodo = {
+        id: records[0].id,
+        ...records[0].fields,
+      };
 
+      if (!records[0].fields.isCompleted) {
+        savedTodo.isCompleted = false;
+      }
+
+      setTodoList([...todoList, savedTodo]);
+    } catch (error) {
+      console.log(error.message);
+      setErrorMessage(error.message);
+    } finally {
+      setIsSaving(false);
+    }
+  };
+
+  const completeTodo = async (id) => {
+    const originalTodo = todoList.find((todo) => todo.id === id);
+
+    const payload = {
+      records: [
+        {
+          id: id,
+          fields: {
+            isCompleted: true,
+          },
+        },
+      ],
+    };
+
+    const options = {
+      method: 'PATCH',
+      headers: {
+        Authorization: token,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    };
+
+    try {
+      const resp = await fetch(url, options);
+
+      if (!resp.ok) {
+        throw new Error(resp.message);
+      }
+
+      const { records } = await resp.json();
+      const completedTodo = {
+        id: records[0]['id'],
+        ...records[0].fields,
+      };
+
+      if (!records[0].fields.isCompleted) {
+        completedTodo.isCompleted = false;
+      }
+
+      const updatedTodos = todoList.map((todo) => {
+        if (todo.id === completedTodo.id) {
+          return { ...completedTodo };
+        }
+        return todo;
+      });
+
+      setTodoList(updatedTodos);
+    } catch (error) {
+      console.log(error.message);
+      setErrorMessage(`${error.message}. Reverting todo...`);
+      // UI reverts to original state if fetch unsuccesful
+      const revertedTodos = originalTodo;
+      setTodoList([...revertedTodos]);
+    } finally {
+      setIsSaving(false);
+    }
+  };
+
+  const updateTodo = async (editedTodo) => {
+    const originalTodo = todoList.find((todo) => todo.id === editedTodo.id);
+
+    const payload = {
+      records: [
+        {
+          id: editedTodo.id,
+          fields: {
+            title: editedTodo.title,
+            isCompleted: editedTodo.isCompleted,
+          },
+        },
+      ],
+    };
+
+    const options = {
+      method: 'PATCH',
+      headers: {
+        Authorization: token,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    };
+
+    try {
+      const resp = await fetch(url, options);
+
+      if (!resp.ok) {
+        throw new Error(resp.message);
+      }
+
+      const { records } = await resp.json();
+      const updatedTodo = {
+        id: records[0]['id'],
+        ...records[0].fields,
+      };
+
+      if (!records[0].fields.isCompleted) {
+        updatedTodo.isCompleted = false;
+      }
+
+      const updatedTodos = todoList.map((todo) => {
+        if (todo.id === updatedTodo.id) {
+          return { ...updatedTodo };
+        }
+        return todo;
+      });
+
+      setTodoList([...updatedTodos]);
+    } catch (error) {
+      console.log(error.message);
+      setErrorMessage(`${error.message}. Reverting todo...`);
+      const revertedTodos = originalTodo;
+      setTodoList([...revertedTodos]);
+    } finally {
+      setIsSaving(false);
+    }
+  };
+
+  // personal note: for the part under TodoList instance to display error, we use the && operator to evaluate errorMessage.
+  // if the state is true (not empty string) it returns condition 2 -> the <div> element, otherwise if is it false (empty) return condition 1.
   return (
     <div>
       <h1>My Todos</h1>
@@ -43,7 +230,16 @@ function App() {
         todoList={todoList}
         onCompleteTodo={completeTodo}
         onUpdateTodo={updateTodo}
+        isLoading={isLoading}
       />
+
+      {errorMessage && (
+        <div>
+          <hr />
+          <p>{errorMessage}</p>
+          <button onClick={() => setErrorMessage('')}>Dismiss Error</button>
+        </div>
+      )}
     </div>
   );
 }
