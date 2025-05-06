069c4a9fcc0fac857fed43558c324049a0ecdade
Fadel Muzahdi <padel.muzahdi@gmail.com>
2025-05-06 02:00:47 -0400

    Fixed update methods of updateTodo and completeTodo from pessimistic to optimistic, ensured error handling works properly, refactored updateTodo and completeTodo to make codebase more succinct

--- src/App.jsx
+++ src/App.jsx
@@ -102,20 +102,23 @@ function App() {
     }
   };
 
-  const completeTodo = async (id) => {
-    const originalTodo = todoList.find((todo) => todo.id === id);
+  const refactorTodo = async (id, fields, updaterFunction) => {
+    const originalTodos = [...todoList];
 
+    // Ensure both have optimistic update
+    setTodoList(updaterFunction);
+
+    // This will change according to each function
     const payload = {
       records: [
         {
-          id: id,
-          fields: {
-            isCompleted: true,
-          },
+          id,
+          fields,
         },
       ],
     };
 
+    // This options remain constant for both functions
     const options = {
       method: 'PATCH',
       headers: {
@@ -129,103 +132,186 @@ function App() {
       const resp = await fetch(url, options);
 
       if (!resp.ok) {
-        throw new Error(resp.message);
-      }
-
-      const { records } = await resp.json();
-      const completedTodo = {
-        id: records[0]['id'],
-        ...records[0].fields,
-      };
-
-      if (!records[0].fields.isCompleted) {
-        completedTodo.isCompleted = false;
-      }
-
-      const updatedTodos = todoList.map((todo) => {
-        if (todo.id === completedTodo.id) {
-          return { ...completedTodo };
-        }
-        return todo;
-      });
-
-      setTodoList(updatedTodos);
-    } catch (error) {
-      console.log(error.message);
-      setErrorMessage(`${error.message}. Reverting todo...`);
-      // UI reverts to original state if fetch unsuccesful
-      const revertedTodos = originalTodo;
-      setTodoList([...revertedTodos]);
-    } finally {
-      setIsSaving(false);
-    }
-  };
-
-  const updateTodo = async (editedTodo) => {
-    const originalTodo = todoList.find((todo) => todo.id === editedTodo.id);
-
-    const payload = {
-      records: [
-        {
-          id: editedTodo.id,
-          fields: {
-            title: editedTodo.title,
-            isCompleted: editedTodo.isCompleted,
-          },
-        },
-      ],
-    };
-
-    const options = {
-      method: 'PATCH',
-      headers: {
-        Authorization: token,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(payload),
-    };
-
-    try {
-      const resp = await fetch(url, options);
-
-      if (!resp.ok) {
-        throw new Error(resp.message);
+        throw new Error(resp.statusText);
       }
 
       const { records } = await resp.json();
       const updatedTodo = {
-        id: records[0]['id'],
+        id: records[0].id,
         ...records[0].fields,
       };
 
-      if (!records[0].fields.isCompleted) {
+      if (!updatedTodo.isCompleted) {
         updatedTodo.isCompleted = false;
       }
 
-      const updatedTodos = todoList.map((todo) => {
-        if (todo.id === updatedTodo.id) {
-          return { ...updatedTodo };
-        }
-        return todo;
-      });
-
-      setTodoList([...updatedTodos]);
+      setTodoList((currentTodoList) =>
+        currentTodoList.map((todo) =>
+          todo.id === updatedTodo.id ? { ...updatedTodo } : todo
+        )
+      );
     } catch (error) {
       console.log(error.message);
       setErrorMessage(`${error.message}. Reverting todo...`);
-      const revertedTodos = originalTodo;
-      setTodoList([...revertedTodos]);
+      setTodoList(originalTodos);
     } finally {
       setIsSaving(false);
     }
   };
 
+  // For personal understanding, these are refactored functions which pass in arguments for the helper function! ex. arg1 (string), arg2 (object), arg3 (functon)
+  const updateTodo = async (editedTodo) => {
+    refactorTodo(
+      editedTodo.id,
+      { title: editedTodo.title, isCompleted: editedTodo.isCompleted },
+      (currentTodoList) =>
+        currentTodoList.map((todo) =>
+          todo.id === editedTodo.id ? { ...todo, ...editedTodo } : todo
+        )
+    );
+  };
+
+  const completeTodo = async (id) => {
+    refactorTodo(id, { isCompleted: true }, (currentTodoList) =>
+      currentTodoList.map((todo) =>
+        todo.id === id ? { ...todo, isCompleted: true } : todo
+      )
+    );
+  };
+
+  // const updateTodo = async (editedTodo) => {
+  //   const originalTodos = [...todoList];
+
+  //   setTodoList(
+  //     todoList.map((todo) =>
+  //       todo.id === editedTodo.id ? { ...todo, ...editedTodo } : todo
+  //     )
+  //   );
+
+  //   const payload = {
+  //     records: [
+  //       {
+  //         id: editedTodo.id,
+  //         fields: {
+  //           title: editedTodo.title,
+  //           isCompleted: editedTodo.isCompleted,
+  //         },
+  //       },
+  //     ],
+  //   };
+
+  //   const options = {
+  //     method: 'PATCH',
+  //     headers: {
+  //       Authorization: token,
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify(payload),
+  //   };
+
+  //   try {
+  //     const resp = await fetch(url, options);
+
+  //     if (!resp.ok) {
+  //       throw new Error(resp.message);
+  //     }
+
+  //     const { records } = await resp.json();
+  //     const updatedTodo = {
+  //       id: records[0]['id'],
+  //       ...records[0].fields,
+  //     };
+
+  //     if (!records[0].fields.isCompleted) {
+  //       updatedTodo.isCompleted = false;
+  //     }
+
+  //     // Utilize same logic as completeTodo
+  //     setTodoList(
+  //       todoList.map((todo) =>
+  //         todo.id === updatedTodo.id ? { ...updatedTodo } : todo
+  //       )
+  //     );
+  //   } catch (error) {
+  //     console.log(error.message);
+  //     setErrorMessage(`${error.message}. Reverting todo...`);
+  //     // UI reverts to original state if fetch unsuccesful
+  //     setTodoList(originalTodos);
+  //   } finally {
+  //     setIsSaving(false);
+  //   }
+  // };
+
+  // const completeTodo = async (id) => {
+  //   const originalTodos = [...todoList];
+
+  //   // Deviating away from the W7 Lesson direction to do optimistic update
+  //   setTodoList(
+  //     todoList.map((todo) =>
+  //       todo.id === id ? { ...todo, isCompleted: true } : todo
+  //     )
+  //   );
+
+  //   const payload = {
+  //     records: [
+  //       {
+  //         id: id,
+  //         fields: {
+  //           isCompleted: true,
+  //         },
+  //       },
+  //     ],
+  //   };
+
+  //   const options = {
+  //     method: 'PATCH',
+  //     headers: {
+  //       Authorization: token,
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify(payload),
+  //   };
+
+  //   try {
+  //     const resp = await fetch(url, options);
+
+  //     if (!resp.ok) {
+  //       throw new Error(resp.message);
+  //     }
+
+  //     const { records } = await resp.json();
+  //     const completedTodo = {
+  //       id: records[0]['id'],
+  //       ...records[0].fields,
+  //     };
+
+  //     // If condition and syncing back from server in case of anomaly inputs
+  //     if (!records[0].fields.isCompleted) {
+  //       completedTodo.isCompleted = false;
+  //     }
+
+  //     setTodoList(
+  //       todoList.map((todo) =>
+  //         todo.id === completedTodo.id ? { ...completedTodo } : todo
+  //       )
+  //     );
+  //   } catch (error) {
+  //     console.log(error.message);
+  //     setErrorMessage(`${error.message}. Reverting todo...`);
+  //     // UI reverts to original state if fetch unsuccesful
+  //     setTodoList(originalTodos);
+  //   } finally {
+  //     setIsSaving(false);
+  //   }
+  // };
+
   // personal note: for the part under TodoList instance to display error, we use the && operator to evaluate errorMessage.
   // if the state is true (not empty string) it returns condition 2 -> the <div> element, otherwise if is it false (empty) return condition 1.
+
   return (
     <div>
       <h1>My Todos</h1>
-      <TodoForm onAddTodo={addTodo} />
+      <TodoForm onAddTodo={addTodo} isSaving={isSaving} />
       <TodoList
         todoList={todoList}
         onCompleteTodo={completeTodo}
